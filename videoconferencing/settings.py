ebed618269e02f41f6cd13c94f784fc1631981a7
Shubham Sharma <121294706+Shubh8009726704@users.noreply.github.com>
2024-01-20 00:58:09 +0530

    Update settings.py

--- videoconferencing/settings.py
+++ videoconferencing/settings.py
@@ -125,3 +125,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = '/path/to/your/static/directory/'
+
